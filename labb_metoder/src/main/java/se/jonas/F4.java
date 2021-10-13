191c940f1b6853d185b3425fb661567cd01fa29f
Jonas324 <jonaswadin24@gmail.com>
2021-10-13 14:34:32 +0200

    hejhej

--- labb_metoder/src/main/java/se/jonas/F4.java
+++ labb_metoder/src/main/java/se/jonas/F4.java
@@ -3,6 +3,14 @@ package se.jonas;
 public class F4 {
 
     public void run() {
+        int a = 25;
+
+        System.out.println("Summan är " + compute(a));
+    }
+
+    private Integer compute(int a) {
+
+        return null;
     }
     
 }
